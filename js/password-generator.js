a9c66ac2eeffed66aaa3fb599a7af05eac2e8332
Bruno César <bruno@agenciaskills.com.br>
2016-09-13 15:57:51 -0300

    Primeiro Commit

---
+++ js/password-generator.js
@@ -0,0 +1,22 @@
+$(function () {
+
+
+
+    $("button[type='button']").click(function(){
+
+    
+    var password = 1234;
+    
+    
+    
+    $("[name='senha']").val(password);
+
+
+    });
+
+
+
+
+
+});
+
